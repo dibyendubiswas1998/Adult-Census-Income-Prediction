00826025c830c9521b2e336d51f3c36da1914ad3
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-10-28 18:17:14 +0530

    it helps to read the params.yaml, create file & directories, delete directories, save the data in local, etc.

---
+++ utils/common_utils.py
@@ -0,0 +1,77 @@
+import yaml
+import os
+import shutil
+
+
+
+def read_params(config_path: str) -> dict:
+    """
+        **Method Name:** read_params\n
+        **Description:** This method helps to read the parameter from params.yaml file\n
+        **Output:** params information\n
+        **On Failure:** Raise Exception\n\n
+
+        :param config_path:
+        :return: params information from params.yaml file.
+    """
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def clean_prev_dirs_if_exis(dir_path: str):
+    """
+        **Method Name:** clean_prev_dirs_if_exis\n
+        **Description:** This method helps to, if any directory is present previously then remove those directories\n
+        **On Failure:** Raise Exception\n\n
+
+        :param dir_path: mention the directory path
+        :return: remove the directory
+    """
+    if os.path.isdir(dir_path):
+        shutil.rmtree(dir_path)
+
+
+def create_dir(dirs: list):
+    """
+        **Method Name:** create_dir\n
+        **Description:** This method helps to create the directory/directories\n
+        **On Failure:** Raise Exception\n\n
+
+        :param dirs: mention the list of directory/directories
+        :return: create directory/directories
+    """
+    for dir in dirs:
+        os.makedirs(dir, exist_ok=True)
+
+def create_file(file_name:str):
+    """
+        **Method Name:** create_file\n
+        **Description:** This method helps to create the file\n
+        **On Failure:** Raise Exception\n\n
+
+        :param file_name: mention file name
+        :return: create the file
+    """
+    with open(file_name, 'w') as file:
+        pass
+
+def save_raw_local_data(data, new_data_path, header=False):
+    """
+        **Method Name:** save_raw_local_df\n
+        **Description:** This method helps to save the data in local directory\n
+        **On Failure:** Raise Exception\n\n
+
+        :param data: data
+        :param new_data_path: mention data path, where you can store the data
+        :param header: columns header
+        :return: save the data
+    """
+    if header:
+        new_col = [col.replace(' ', "_") for col in data.columns]
+        data.to_csv(new_data_path, index=False, header=new_col)
+    else:
+        data.to_csv(new_data_path, index=False)
+
+
+if __name__ == "__main__":
+    pass
