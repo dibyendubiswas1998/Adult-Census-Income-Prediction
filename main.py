b2f8b0ae4746b3163b4776e8c3cc7453d37b0052
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-11-11 19:24:32 +0530

    update all

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from load_and_split_data.load_split import load_split
+from training_raw_data_validation.rawdataValidation import RawDataValidation
+import utils.common_utils as common_utils
+import pandas as pd
 import argparse
 
 if __name__ == "__main__":
+    config = common_utils.read_params('params.yaml')
+    train_path = config['artifacts']['split_data']['train_path']
+    data = pd.read_csv(train_path, sep=',')
+
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
@@ -14,6 +21,7 @@ if __name__ == "__main__":
 
 
         # stages: training_raw_data_validation
+        rw = RawDataValidation(config_path=parsed_args.config)
 
 
     except Exception as ex:
